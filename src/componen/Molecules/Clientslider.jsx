fc614d19cc727094ed2f57f4e6003a76dcb655aa
Abdo90800 <zezocr7missi@gmail.com>
2024-11-20 19:46:16 +0200

    done

--- src/componen/Molecules/Clientslider.jsx
+++ src/componen/Molecules/Clientslider.jsx
@@ -8,28 +8,33 @@ import CardComponent from "../atoms/Clientslid";
 
 const Clientslider = ({ cardsData }) => {
   return (
-    <Swiper
-      modules={[Navigation, Pagination]}
-      navigation
-      pagination={{ clickable: true }}
-      spaceBetween={30}
-      slidesPerView={3}
-      breakpoints={{
-        640: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-      }}
-      className="max-w-screen-xl px-2 mx-auto"
-    >
-      {cardsData.map((product, index) => (
-        <SwiperSlide key={index}>
-          <CardComponent product={product} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="swiper-container-wrapper">
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation
+        pagination={{ clickable: true }}
+        spaceBetween={30}
+        slidesPerView={3}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+        }}
+        className="swiper-container"
+      >
+        {cardsData.map((product, index) => (
+          <SwiperSlide key={index}>
+            <CardComponent product={product} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="custom-pagination">
+        {/* هنا يمكن إضافة أي عنصر خاص بالنقاط */}
+      </div>
+    </div>
   );
 };
 
